3abeaf65c5d31ada414dedf529c113c79277f25e
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-03-17 15:47:16 +0000

    wrote in the renderLicenseBadge function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,20 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+//created an array of objects for each license and with 2 properties each: badge name and badge url
+  const licenseBadges = [
+    {badgeName : 'MIT', badgeUrl : '(https://img.shields.io/badge/License-MIT-yellow.svg)'},
+    {badgeName :'APACHE 2.0', badgeUrl :'(https://img.shields.io/badge/License-Apache%202.0-blue.svg)'},
+    {badgeName :'GPL 3.0', badgeUrl : '(https://img.shields.io/badge/license-GPL%203.0-blue.svg)'},
+    {badgeName :'BSD 3', badgeUrl : '(https://img.shields.io/badge/license-BSD%203-blue.svg)'},
+    {badgeName :'None', badgeUrl : ''}
+]
+
+//used .find() to iterate through the array and return the first badgeName that matches the seleceted license
+const selectedLicense = licenseBadges.find((badge)=> license === badge.badgeName)
+return selectedLicense ? selectedLicense.badgeUrl : ''
+//checks if selectedLicense is valid, if so it's url is returned if selectedLicense isnt valid an empty string is returned
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
