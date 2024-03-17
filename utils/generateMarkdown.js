8590c3c308ddfb01badf49a0a32a34161674568a
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-03-17 16:13:19 +0000

    completed the renderLicenseLink function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,24 +1,36 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
+//license = data.license (ie the license the user selected)
 function renderLicenseBadge(license) {
 //created an array of objects for each license and with 2 properties each: badge name and badge url
   const licenseBadges = [
-    {badgeName : 'MIT', badgeUrl : '(https://img.shields.io/badge/License-MIT-yellow.svg)'},
-    {badgeName :'APACHE 2.0', badgeUrl :'(https://img.shields.io/badge/License-Apache%202.0-blue.svg)'},
-    {badgeName :'GPL 3.0', badgeUrl : '(https://img.shields.io/badge/license-GPL%203.0-blue.svg)'},
-    {badgeName :'BSD 3', badgeUrl : '(https://img.shields.io/badge/license-BSD%203-blue.svg)'},
-    {badgeName :'None', badgeUrl : ''}
+    {name : 'MIT', badgeUrl : 'https://img.shields.io/badge/License-MIT-yellow.svg'},
+    {name :'APACHE 2.0', badgeUrl :'https://img.shields.io/badge/License-Apache%202.0-blue.svg'},
+    {name :'GPL 3.0', badgeUrl : 'https://img.shields.io/badge/license-GPL%203.0-blue.svg'},
+    {name :'BSD 3', badgeUrl : 'https://img.shields.io/badge/license-BSD%203-blue.svg'},
+    {name :'None', badgeUrl : ''}
 ]
 
 //used .find() to iterate through the array and return the first badgeName that matches the seleceted license
-const selectedLicense = licenseBadges.find((badge)=> license === badge.badgeName)
+const selectedLicense = licenseBadges.find((badge)=> license === badge.name)
 return selectedLicense ? selectedLicense.badgeUrl : ''
 //checks if selectedLicense is valid, if so it's url is returned if selectedLicense isnt valid an empty string is returned
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  //created an array of objects for each license and with 2 properties each: license name and license link
+  const licenseLinks = [
+    {name : 'MIT', licenseLink : 'https://opensource.org/license/mit'},
+    {name :'APACHE 2.0', licenseLink :'https://www.apache.org/licenses/LICENSE-2.0'},
+    {name :'GPL 3.0', licenseLink : 'https://www.gnu.org/licenses/gpl-3.0.en.html'}, 
+    {name :'BSD 3', licenseLink : 'https://opensource.org/license/bsd-3-clause'},
+    {name :'None', licenseLink : ''}
+]
+const selectedLicense = licenseLinks.find((badge)=> license === badge.name)
+return (selectedLicense) ? selectedLicense.licenseLink : ''
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
